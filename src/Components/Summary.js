07dd2631e8d725d2c475ca174862d1bed0fdb405
Shreyash Kaushal <shreyash@nalashaa.net>
2024-05-20 14:01:07 +0530

    summary static html implemented

--- src/Components/Summary.js
+++ src/Components/Summary.js
@@ -1,8 +1,54 @@
+import React, { useState } from 'react';
+import '../Style/Summary.css';
+import Button from '@mui/material/Button';
+
 function Summary() {
     return (
-        <>
-         <h3>Summary</h3>
-        </>
+        <div className="parent-container">
+            <div className='container'>
+                <div className='heading-div'>Profitability</div>
+                <div className='summary-container'>
+                    <div>
+                        <div className="profit-div">
+                            <div className='profit-label'>Total Price</div>
+                            <div>$0.00</div>
+                        </div>
+                        <div className="profit-div">
+                            <div className='profit-label'>ATG Discount</div>
+                            <div>$0.00</div>
+                        </div>
+                        <div className="profit-div">
+                            <div className='profit-label'>Total Tax</div>
+                            <div>$0.00</div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="profit-div">
+                            <div className='profit-label'>Total Amount</div>
+                            <div>$0.00</div>
+                        </div>
+                        <div className="profit-div">
+                            <div className='profit-label'>Total Cost</div>
+                            <div>$0.00</div>
+                        </div>
+                        <div className="profit-div">
+                            <div className='profit-label'>Profit</div>
+                            <div>$0.00</div>
+                        </div>
+                        <div className="profit-div">
+                            <div className='profit-label'>Shipping</div>
+                            <div>$0.00</div>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div className="button-container">
+                    <Button variant="contained">Back</Button>
+                    <Button variant="contained">Update</Button>
+                </div>
+            </div>
+        </div>
     );
 }
 
