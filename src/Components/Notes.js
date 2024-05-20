5e2b58fb9b491e192b88252258753ad6be37037a
Shreyash Kaushal <shreyash@nalashaa.net>
2024-05-20 11:59:43 +0530

    Notes static html as per prod design done

--- src/Components/Notes.js
+++ src/Components/Notes.js
@@ -1,8 +1,55 @@
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 function Notes() {
     return (
-        <>
-         <h3>Notes</h3>
-        </>
+        <div className="parent-container">
+            <div className='container'>
+                <div className='heading-div'>External Notes</div>
+                <div className='container'>
+                    <div className='input-container'>
+                        <TextField
+                            id="standard-multiline-flexible"
+                            label="External Notes (Notes that will appear on the quote)"
+                            InputProps={{
+                                rows: 6,
+                                multiline: true,
+                                inputComponent: 'textarea'
+                            }}
+                            variant="standard"
+                        />
+                        <TextField
+                            id="standard-multiline-flexible"
+                            label="Internal Notes(Does not Appear on Quote)"
+                            InputProps={{
+                                rows: 6,
+                                multiline: true,
+                                inputComponent: 'textarea'
+                            }}
+                            variant="standard"
+                        />
+
+
+                        <TextField
+                            id="standard-multiline-flexible"
+                            label="Invoice Notes (Information that shows on the invoice)"
+                            InputProps={{
+                                rows: 6,
+                                multiline: true,
+                                inputComponent: 'textarea'
+                            }}
+                            variant="standard"
+                        />
+                    </div>
+                </div>
+                <div className="button-container">
+                    <Button variant="contained">Back</Button>
+                    <Button variant="contained">Next</Button>
+                </div>
+
+            </div>
+        </div>
     );
 }
 
